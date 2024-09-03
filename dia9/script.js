119c1a8791f585c6a6e87d62627e352374fafdae
FreilerOrtega <aleiroestupinan1999@gmail.com>
2024-09-02 22:08:14 -0500

    feat:sparkles: actualizacion pokemongo

--- dia9/script.js
+++ dia9/script.js
@@ -1,12 +1,28 @@
-document.getElementById("input1").addEventListener("input",()=>{
+document.getElementById("input1").addEventListener("change",()=>{
     let valor = document.getElementById("input1").value
     console.log(valor)
 
-    var api="https://pokeapi.co/api/"
+    
+    var api="https://pokeapi.co/api/v2/pokemon/"
 
     fetch(api+valor)
     .then(res=>res.json())
     .then(pokemon=>{
+      let img = document.getElementById("muneco");
+      img.innerHTML=`
+      <img src="${pokemon.sprites.other.showdown.front_default}">
+     `
+     let nombreid = document.getElementById("tabla");
+     nombreid.innerHTML=`
+     <tr>
+        <td>${pokemon.id}-</td>
+        <td>${pokemon.name}</td>
+      </tr>
+     `
+     
+      
         
     })
+   
+    
 })
