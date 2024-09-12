6d0f96daaec0ad4ab9c7dea3fc2c005768b8eeeb
FreilerOrtega <aleiroestupinan1999@gmail.com>
2024-09-12 13:00:28 -0500

    :sparkles: poker

--- dia13/script.js
+++ dia13/script.js
@@ -1,11 +1,32 @@
-const api=`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
+const api = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
 
 
-    
-    fetch(api)
-        .then(response => response.json())
-        .then(data => {
-            
-            console.log(data);
-        })
-            
+let jugador1 = document.getElementById("jugador_1")
+
+
+
+fetch(api)
+    .then(response => response.json())
+    .then(data => {
+
+        console.log(data);
+
+        let baraja = data.deck_id
+
+        fetch(`https://deckofcardsapi.com/api/deck/${baraja}/draw/?count=2`)
+            .then(response => response.json()
+            ).then(carta => {
+
+                console.log(carta);
+
+                let guardarmundo = document.getElementById("img");
+                guardarmundo.innerHTML = `<div id="jugador1">
+                <img id="imgcard" src="${carta.cards[0].image}" alt="">
+                
+            </div>`
+              
+
+            })
+    })
+
+
